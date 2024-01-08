d766a4e51fa23823bcb7e6bee79f5366def5d52f
uvreddymsm@gmail.com <12Test@12>
2024-01-08 09:39:42 +0530

    ChromeDriverAdded

--- src/module/launchapplication.java
+++ src/module/launchapplication.java
@@ -6,25 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class launchapplication {
-	
+
 	public static WebDriver driver;
-	
-	
-	
-	@Test
-	public void TC1()  {
-		// TODO Auto-generated method stub
 
-	
 
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Library\\chromedriver.exe");
-		driver=new ChromeDriver();
-		driver.get("https://www.flipkart.com/");
-		driver.manage().window().maximize();
-//		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("IPHONE");
-//		driver.findElement(By.id("nav-search-submit-button")).click();
-		System.out.println("Test Case Executed Completed Buddy");
-	}
+
+//	@Test
+//	public void TC1()  {
+//		// TODO Auto-generated method stub
+//
+////
+////		System.setProperty("webdriver.chrome.driver", "D:\\NewAutomation\\TestingApp\\Library\\chromedriver.exe");
+////		//	System.setProperty("webdriver.chrome.driver", "./Library/chromedriver.exe");
+////		//	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Library\\chromedriver.exe");
+////		driver=new ChromeDriver();
+////		driver.get("https://www.flipkart.com/");
+////		driver.manage().window().maximize();
+//		System.out.println("Test Case Executed Completed Buddy");
+//	}
 
 	@Test
 	public void TC2() {
